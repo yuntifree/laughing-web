2ed34657abb12ceff7b17d82ab33f592a862995c
hzgnpu <tinylee@qq.com>
2017-05-04 17:52:03 +0800

    add share htmls

---
+++ app/share/js/data.js
@@ -0,0 +1,62 @@
+var facebook = {
+  list:[{
+    text: 'Click the arrow button when you see a laugh video in Facebook.',
+    img: './images/Facebook1.png'
+  },
+  {
+    text: 'Click the “Copy Link” button.',
+    img: './images/Facebook2.png'
+  },
+  {
+    text: 'Open the Laughing app，then you will see the share tips.',
+    img: './images/videolink.png'
+  }]
+}
+var youtobe = {
+  list:[{
+    text: 'Click the “share” button when you see a laugh video in YouTobe.',
+    img: './images/YouTobe1.png'
+  },
+  {
+    text: 'Click the “Copy Link” button.',
+    img: './images/YouTobe2.png'
+  },
+  {
+    text: 'Open the Laughing app，then you will see the share tips.',
+    img: './images/videolink.png'
+  }]
+}
+var muse = {
+  list:[{
+    text: 'Click the “share” button when you see a laugh video in YouTobe.',
+    img: './images/muse1.png'
+  },
+  {
+    text: 'Click the “Copy Link” button.',
+    img: './images/muse2.png'
+  },
+  {
+    text: 'Open the Laughing app，then you will see the share tips.',
+    img: './images/videolink.png'
+  }]
+}
+var instagram = {
+  list:[{
+    text: 'Click the “share” button when you see a laugh video in YouTobe.',
+    img: './images/instagram1.png'
+  },
+  {
+    text: 'Click the “Copy Link” button.',
+    img: './images/instagram2.png'
+  },
+  {
+    text: 'Open the Laughing app，then you will see the share tips.',
+    img: './images/videolink.png'
+  }]
+}
+
+
+window.facebook = facebook;
+window.youtobe = youtobe;
+window.muse = muse;
+window.instagram = instagram;
