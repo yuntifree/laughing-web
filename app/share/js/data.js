48cb36cfc9a5b7349aabc5a7da8de30a04f5b40a
chunyanzeng <2218583181@qq.com>
2017-05-15 15:37:51 +0800

    img url

--- app/share/js/data.js
+++ app/share/js/data.js
@@ -1,57 +1,57 @@
 var facebook = {
   list:[{
     text: 'Click the arrow button when you see a laugh video in Facebook.',
-    img: './images/Facebook1.png'
+    img: 'http://laugh.us-ca.ufileos.com/75273677-6028-4351-b7cf-49ed1dea1017.jpg'
   },
   {
     text: 'Click the “Copy Link” button.',
-    img: './images/Facebook2.png'
+    img: 'http://laugh.us-ca.ufileos.com/783f5389-e45f-4583-9779-04b200dcb1c7.jpg'
   },
   {
     text: 'Open the Laughing app，then you will see the share tips.',
-    img: './images/videolink.png'
+    img: 'http://laugh.us-ca.ufileos.com/b5b7bf89-fb0e-4eae-9184-c80ebd4bf2d7.jpg'
   }]
 }
-var youtobe = {
+var youtube = {
   list:[{
-    text: 'Click the “share” button when you see a laugh video in YouTobe.',
-    img: './images/YouTobe1.png'
+    text: 'Click the “share” button when you see a laugh video in YouTube.',
+    img: 'http://laugh.us-ca.ufileos.com/41a4230f-2ce6-4a58-b75c-505acce04b64.jpg'
   },
   {
     text: 'Click the “Copy Link” button.',
-    img: './images/YouTobe2.png'
+    img: 'http://laugh.us-ca.ufileos.com/bed214d1-0dd4-44c2-ac7a-583691c10db6.jpg'
   },
   {
     text: 'Open the Laughing app，then you will see the share tips.',
-    img: './images/videolink.png'
+    img: 'http://laugh.us-ca.ufileos.com/b5b7bf89-fb0e-4eae-9184-c80ebd4bf2d7.jpg'
   }]
 }
 var muse = {
   list:[{
     text: 'Click the “share” button when you see a laugh video in muse.',
-    img: './images/muse1.png'
+    img: 'http://laugh.us-ca.ufileos.com/4cc5d07d-9e40-4718-b32f-9fe50506e5df.jpg'
   },
   {
     text: 'Click the “Copy Link” button.',
-    img: './images/muse2.png'
+    img: 'http://laugh.us-ca.ufileos.com/4288d240-fafc-41a1-ae2f-28102e3e4563.jpg'
   },
   {
     text: 'Open the Laughing app，then you will see the share tips.',
-    img: './images/videolink.png'
+    img: 'http://laugh.us-ca.ufileos.com/b5b7bf89-fb0e-4eae-9184-c80ebd4bf2d7.jpg'
   }]
 }
 var instagram = {
   list:[{
     text: 'Click the “share” button when you see a laugh video in Instagram.',
-    img: './images/instagram1.png'
+    img: 'http://laugh.us-ca.ufileos.com/a5d980e8-47de-40b3-9e7c-a178f6dbeb96.jpg'
   },
   {
     text: 'Click the “Copy Link” button.',
-    img: './images/instagram2.png'
+    img: 'http://laugh.us-ca.ufileos.com/befc9869-cb8b-4698-9df9-01d4e33b6cc4.jpg'
   },
   {
     text: 'Open the Laughing app，then you will see the share tips.',
-    img: './images/videolink.png'
+    img: 'http://laugh.us-ca.ufileos.com/8c189dbe-aacb-4bcf-a1df-460fe09e3d3e.jpg'
   }]
 }
 
